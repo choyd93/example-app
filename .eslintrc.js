ca6bc6c20ada4f070af478acb0554f1fb6c6986f
youngdongCho <youngdong.cho@domfam.com>
2023-07-12 17:53:15 +0900

    chore: 개발 초기 세팅

---
+++ .eslintrc.js
@@ -0,0 +1,72 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "project": [
+            "./tsconfig.json"
+        ]
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/typescript",
+        "airbnb",
+        "airbnb-typescript",
+        "prettier"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true,
+                "es2021": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "react/react-in-jsx-scope": "off",
+        "spaced-comment": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "arrow-body-style": ["error", "as-needed"],
+        "import/prefer-default-export": "off",
+        "no-nested-ternary": "off",
+        "@typescript-eslint/naming-convention": [
+            "error",
+            {
+                "selector": "variable",
+                "format": ["camelCase", "snake_case", "UPPER_CASE", "PascalCase"]
+            }
+        ],
+        "react/function-component-definition":[
+            2,
+            { "namedComponents": [
+                    "arrow-function",
+                    "function-declaration"
+                ] }
+        ],
+        "react/jsx-filename-extension": [
+            1,
+            {
+                "extensions": [
+                    ".js",
+                    ".jsx",
+                    ".tsx"
+                ]
+            }
+        ]
+    }
+}
