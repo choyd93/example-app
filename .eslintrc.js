eeb776170506a47459669bb6bd1e47553e2ac3ff
youngdongCho <youngdong.cho@domfam.com>
2023-07-12 18:30:46 +0900

    style: global style 추가 및 eslint 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,72 +1,60 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json"],
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript",
+    "airbnb",
+    "airbnb-typescript",
+    "prettier",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+        es2021: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
     },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "project": [
-            "./tsconfig.json"
-        ]
-    },
-    "extends": [
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:import/typescript",
-        "airbnb",
-        "airbnb-typescript",
-        "prettier"
+  ],
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "spaced-comment": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "arrow-body-style": ["error", "as-needed"],
+    "import/prefer-default-export": "off",
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+    "no-nested-ternary": "off",
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: "variable",
+        format: ["camelCase", "snake_case", "UPPER_CASE", "PascalCase"],
+      },
     ],
-    "overrides": [
-        {
-            "env": {
-                "node": true,
-                "es2021": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
+    "react/function-component-definition": [
+      2,
+      { namedComponents: ["arrow-function", "function-declaration"] },
     ],
-    "plugins": [
-        "react",
-        "@typescript-eslint"
+    "react/jsx-filename-extension": [
+      1,
+      {
+        extensions: [".js", ".jsx", ".tsx"],
+      },
     ],
-    "rules": {
-        "react/react-in-jsx-scope": "off",
-        "spaced-comment": "off",
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "arrow-body-style": ["error", "as-needed"],
-        "import/prefer-default-export": "off",
-        "no-nested-ternary": "off",
-        "@typescript-eslint/naming-convention": [
-            "error",
-            {
-                "selector": "variable",
-                "format": ["camelCase", "snake_case", "UPPER_CASE", "PascalCase"]
-            }
-        ],
-        "react/function-component-definition":[
-            2,
-            { "namedComponents": [
-                    "arrow-function",
-                    "function-declaration"
-                ] }
-        ],
-        "react/jsx-filename-extension": [
-            1,
-            {
-                "extensions": [
-                    ".js",
-                    ".jsx",
-                    ".tsx"
-                ]
-            }
-        ]
-    }
-}
+  },
+};
